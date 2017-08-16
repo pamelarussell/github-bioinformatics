094a7848d46fdc49cdd11131bc6e6f0b21797f6a
pamelarussell <pamela.russell@gmail.com>
2017-08-16 14:58:11 -0600

    reorganize exploratory analysis

---
+++ src/R/exploratory_analysis/project_info.R
@@ -0,0 +1,16 @@
+
+# Project info
+proj <- "github-bioinformatics-157418"
+ds_gh <- "test_repos"
+ds_analysis <- "test_repos_analysis_results"
+ds_lang <- "languages"
+table_loc_by_repo <- "lines_of_code_by_repo"
+table_code_chunk_freq_10_50 <- "code_chunk_freq_by_repo_10_50"
+table_code_chunk_freq_5_80 <- "code_chunk_freq_by_repo_5_80"
+table_com <- "source_code_comments" # Comments table
+table_loc <- "lines_of_code_by_file" # Lines of code table
+table_art <- "articles_by_repo"
+table_lang <- "languages"
+table_pr <- "pull_requests"
+table_repo_info <- "repo_info_gh_api"
+
