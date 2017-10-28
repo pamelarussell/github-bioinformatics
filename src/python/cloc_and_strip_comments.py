85038d10ccb82c4188f7199091e2cfc2602b39e2
pamelarussell <pamela.russell@gmail.com>
2017-10-28 14:56:38 -0600

    list of dicts

--- src/python/cloc_and_strip_comments.py
+++ src/python/cloc_and_strip_comments.py
@@ -225,7 +225,7 @@ for contents_blob in contents_blobs:
             push_bq_records(bq_client, out_ds, table_loc_ungrouped, recs_to_add_loc)
             push_bq_records(bq_client, out_ds, table_sc_ungrouped, recs_to_add_sc)
         if len(skipped_sha) > 0:
-            push_bq_records(bq_client, out_ds, table_skip, {'sha': sha for sha in skipped_sha})
+            push_bq_records(bq_client, out_ds, table_skip, [{'sha': sha for sha in skipped_sha}])
 
         # Delete the temporary CSV file
         print("Deleting temporary file %s" % contents_csv)
