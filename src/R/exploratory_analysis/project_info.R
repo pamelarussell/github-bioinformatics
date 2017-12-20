fb1919bb510250b358488ea92f596b4644f740b5
pamelarussell <pamela.russell@gmail.com>
2017-12-20 11:26:08 -0700

    more repo-level covariates

--- src/R/exploratory_analysis/project_info.R
+++ src/R/exploratory_analysis/project_info.R
@@ -1,20 +1,36 @@
 
-# Project info
+# Project name
 proj <- "github-bioinformatics-171721"
+
+# Data from GitHub API
 ds_gh <- "repos"
-ds_analysis <- "analysis"
-ds_lang <- "languages"
-table_loc_by_repo <- "lines_of_code_by_repo"
-table_code_chunk_freq_10_50 <- "code_chunk_freq_by_repo_10_50"
-table_code_chunk_freq_5_80 <- "code_chunk_freq_by_repo_5_80"
-table_com <- "source_code_comments" # Comments table
-table_loc <- "lines_of_code_by_file" # Lines of code table
-table_art <- "articles_by_repo"
-table_lang <- "languages"
 table_pr <- "pull_requests"
 table_repo_info <- "repo_metrics"
 table_init_commit <- "file_init_commit"
+table_licenses <- "licenses"
+table_repo_metrics <- "repo_metrics"
+table_file_info <- "file_info"
+
+# Data from analysis results
+ds_analysis <- "analysis"
+table_loc_by_repo <- "lines_of_code_by_repo"
+table_code_chunk_freq_10_50 <- "code_chunk_freq_by_repo_10_50"
+table_code_chunk_freq_5_80 <- "code_chunk_freq_by_repo_5_80"
+table_loc <- "lines_of_code_by_file"
 table_commit_types <- "commit_types"
+table_proj_duration <- "project_duration"
+table_num_langs_by_repo <- "num_langs_by_repo"
+table_num_devs_by_repo <- "num_devs_by_repo"
+
+# Programming languages
+ds_lang <- "languages"
+
+# Lit search
+ds_lit_search <- "lit_search"
+table_repo_and_article <- "repo_and_article_curated"
+
+
+
 
 
 
