b5b35741407578e091a67eac9c392559a7ea6753
pamelarussell <pamela.russell@gmail.com>
2017-12-23 08:07:31 -0700

    test cases

--- src/R/exploratory_analysis/project_info.R
+++ src/R/exploratory_analysis/project_info.R
@@ -22,6 +22,7 @@ table_proj_duration <- "project_duration"
 table_num_langs_by_repo <- "num_langs_by_repo"
 table_num_devs_by_repo <- "num_devs_by_repo"
 table_lang_bytes_by_repo <- "language_bytes_by_repo"
+table_test_cases <- "test_cases"
 
 # Programming languages
 ds_lang <- "languages"
